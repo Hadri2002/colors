4f51acb695a0b46c974be217a22614749209bc99
Hadri2002 <horvathadri006@gmail.com>
2023-04-28 19:15:27 +0200

    guess base

--- app/Guess/Guess.js
+++ app/Guess/Guess.js
@@ -7,6 +7,21 @@ export default class Guess extends Application{
      */
     containerElem;
 
+    /**
+     * @type {HTMLElement}
+     */
+    rand;
+
+    /**
+     * @type {HTMLElement}
+     */
+    guesses;
+
+    /**
+     * @type {HTMLElement}
+     */
+    input;
+
     init() {
         super.init();
         this.initDom();
@@ -21,13 +36,57 @@ export default class Guess extends Application{
 
         const color = new Color([r,g,b]);
         console.log(color.rgb);
-        this.containerElem.appendChild(color.domElem);
+        this.rand.appendChild(color.domElem);
     }
 
     initDom(){
         this.containerElem = document.createElement('div');
+        this.rand = document.createElement('div');
+        this.containerElem.appendChild(this.rand);
+
+        this.guesses = document.createElement('div');
+        this.guesses.className = "color-guesses";
+        this.rand.appendChild(this.guesses);    
+        
+        
+        this.input = document.createElement('div');
+        this.input.className = "guess-input";
+        this.containerElem.appendChild(this.input);
+
+        for(let elem of ['Red', 'Green', 'Blue']){
+            
+            const range = document.createElement('input')
+            this.input.appendChild(range);
+            range.type = "range";
+            range.name = elem.toLowerCase();
+            range.value = "0";
+            range.min = "0";
+            range.max = "255";
+
+            
+            this.input.appendChild(document.createElement('label'));
+            this.input.lastChild.setAttribute('for', elem.toLowerCase());
+            this.input.lastChild.className = "label" + elem.toLowerCase();
+            this.input.lastChild.innerHTML = range.value;
+            
+            range.addEventListener('input', function(){
+                range.parentElement.querySelector(".label" + elem.toLowerCase()).innerHTML = range.value
+            });
+        }
+
+        this.input.appendChild(document.createElement('button'));
+        this.input.lastChild.innerHTML = "Submit";
+
+        //ide majd az adatok átadása kell és a játék logikája
+        this.input.lastChild.addEventListener('click', function(){
+            //NEM MŰKÖDIK A BIND, ELEGEM VAN
+        }.bind(this));
         
         this.target.appendChild(this.containerElem);
+    }
+
+    makeGuess(){
+
     }
 
 }
